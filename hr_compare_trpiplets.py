33e14143fc07dc94e8a832ebafc9adf0ef3b5db9
Alex Nogales <a.nogales.cs@gmail.com>
2021-03-17 12:49:19 -0300

    add hr_compare_triplets.py

---
+++ hr_compare_trpiplets.py
@@ -0,0 +1,32 @@
+'''
+Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, 
+awarding points on a scale from 1 to 100 for three categories: problem clarity, originality, and difficulty.
+
+The rating for Alice's challenge is the triplet 
+a = (a[0], a[1], a[2]), and the rating for Bob's challenge is the triplet b = (b[0], b[1], b[2]).
+
+The task is to find their comparison points by comparing a[0] with b[0], a[1] with b[1], and a[2] with b[2].
+
+If a[i] > b[i], then Alice is awarded 1 point.
+If a[i] < b[i], then Bob is awarded 1 point.
+If a[i] = b[i], then neither person receives a point.
+Comparison points is the total points a person earned.
+
+Given a and b, determine their respective comparison points.
+'''
+
+a = [1, 2, 3]
+b = [3, 2, 1]
+
+def compare_triplets(a, b):
+    a_score, b_score = 0, 0 
+    for index, _ in enumerate(a):
+        if a[index] > b[index]:
+            a_score += 1
+        elif a[index] < b[index]:
+            b_score += 1
+
+    return a_score, b_score
+
+if __name__ == '__main__':
+    print(compare_triplets(a, b))
