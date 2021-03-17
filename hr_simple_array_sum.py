3a75c869fdf79c41e6ee032369c87932f8a8d768
Alex Nogales <a.nogales.cs@gmail.com>
2021-03-17 12:38:08 -0300

    add hr_simple_array_sum.py

---
+++ hr_simple_array_sum.py
@@ -0,0 +1,11 @@
+'''
+Given an array of integers, find the sum of its elements.
+'''
+arr = [1, 2, 4]
+def simple_array_sum(arr):
+    return sum(arr)
+
+
+
+if __name__ == '__main__':
+    print(simple_array_sum(arr))
